2f19fc46ad8ef24f8d1b128b25acdd1827b71c9d
DracoR22 <rdraco039@gmail.com>
2023-06-09 12:11:20 -0500

    Update

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -32,7 +32,6 @@ const AuthModal = () => {
    <Modal title="Welcome back" description="Login to your account" isOpen={isOpen} onChange={onChange}>
      <Auth 
      theme="dark"
-     magicLink
      providers={['github']}
      supabaseClient={supabaseClient}
      appearance={{theme: ThemeSupa, variables: {default: {colors: {brand: '#404040', brandAccent: '#22c55e'}}}}}/>
